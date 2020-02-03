a4a6a079b3ffa8a5e4fe3729c336f51952870785
CodingCCarpenter <christineassists@aol.com>
2020-02-02 20:11:35 -0600

    christine-carpenter

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const carsRouter = require('../car-dealer/cars-router.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.use('/api/car-dealer', carsRouter);
+
+module.exports = server;
