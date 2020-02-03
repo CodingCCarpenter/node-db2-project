6e7b7e622ce2aecec609776e6f07c03935c21788
CodingCCarpenter <christineassists@aol.com>
2020-02-02 21:51:31 -0600

    add useNullAsDefault to knexfile

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,11 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: './dev.sqlite3'
+    },
+    useNullAsDefault: true,
+    // generates migration files in a data/migrations/ folder
+    migrations: {
+      directory: './data/migrations'
     }
   },
 
